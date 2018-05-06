1fd8b4e4ae84005b35b5f9d19acfe14e1e7b21e7
lermanori <lermanori@gmail.com>
2018-05-06 18:52:31 +0300

    checkers GameStep StyleCoped

--- B18 Ex02 Ori 205665508 Amir 305707044/Checkers_LogicAndDataSection/CheckersGameStep.cs
+++ B18 Ex02 Ori 205665508 Amir 305707044/Checkers_LogicAndDataSection/CheckersGameStep.cs
@@ -4,21 +4,21 @@ namespace Checkers_LogicAndDataSection
 {
     public struct CheckersGameStep
     {
-
         public struct MoveType
         {
             private bool m_KingMove;
-            private eMoveTypes m_moveType;
+            private eMoveTypes m_MoveType;
 
-            public eMoveTypes moveType
+            public eMoveTypes TypeIndicator
             {
-                get { return m_moveType; }
+                get { return m_MoveType; }
                 set
                 {
-                    m_moveType = value;
+                    m_MoveType = value;
                 }
             }
-            public bool kingMove
+
+            public bool KingMove
             {
                 get { return m_KingMove; }
                 set
@@ -27,18 +27,15 @@ namespace Checkers_LogicAndDataSection
                 }
             }
 
-            public static MoveType initalize()
+            public static MoveType Initalize()
             {
                 MoveType result = new MoveType();
 
-                result.m_moveType = eMoveTypes.Undefined;
+                result.m_MoveType = eMoveTypes.Undefined;
                 result.m_KingMove = false;
 
                 return result;
-
             }
-
-
 
             public static MoveType CalculateMoveType(CheckersGameStep i_requestedStep)
             {
@@ -52,27 +49,26 @@ namespace Checkers_LogicAndDataSection
                 distanceY = Abs(i_requestedStep.RequestedPosition.YCooord - i_requestedStep.CurrentPosition.YCooord);
                 distanceX = Abs(i_requestedStep.RequestedPosition.XCoord - i_requestedStep.CurrentPosition.XCoord);
 
-
                 if (distanceY == 2 && distanceX == 2)
                 {
-                    result.m_moveType = eMoveTypes.EatMove;
+                    result.m_MoveType = eMoveTypes.EatMove;
                 }
                 else if (distanceY == 1 && distanceX == 1)
                 {
-                    result.m_moveType = eMoveTypes.RegularMove;
+                    result.m_MoveType = eMoveTypes.RegularMove;
                 }
                 else
                 {
-                    result.m_moveType = eMoveTypes.Undefined;
+                    result.m_MoveType = eMoveTypes.Undefined;
                 }
+
                 if (distanceX > 2 || distanceY > 2 || distanceX < 1 || distanceY < 1)
                 {
-                    result.m_moveType = eMoveTypes.Undefined;
+                    result.m_MoveType = eMoveTypes.Undefined;
                 }
 
                 switch (SessionData.m_currentActivePlayer)
                 {
-
                     case ePlayerOptions.Player1:
                         indexForLastLineOnBoard = 0;
                         break;
@@ -82,43 +78,43 @@ namespace Checkers_LogicAndDataSection
                         indexForLastLineOnBoard = (int)SessionData.m_BoardSize - 1;
                         break;
                 }
+
                 if (i_requestedStep.RequestedPosition.YCooord == indexForLastLineOnBoard)
                 {
-                    result.kingMove = true;
+                    result.KingMove = true;
                 }
                 else
                 {
-                    result.kingMove = false;
+                    result.KingMove = false;
                 }
 
                 return result;
-
-
             }
         }
+
         private Point m_currentSoldierPosition;
         private Point m_requestedSoldierPosition;
         private MoveType m_moveInfo;
-        private bool m_quit;
+        private bool m_quitIndicator; 
 
-        public static CheckersGameStep CreateCheckersGameStep(Point i_currentSoldierPosition, Point i_requestedSoldierPosition, bool ToQuit = false)
+        public static CheckersGameStep CreateCheckersGameStep(Point i_currentSoldierPosition, Point i_requestedSoldierPosition, bool i_ToQuit = false)
         {
             CheckersGameStep result = new CheckersGameStep();
 
             result.CurrentPosition = i_currentSoldierPosition;
             result.RequestedPosition = i_requestedSoldierPosition;
-            result.moveTypeInfo = MoveType.CalculateMoveType(result);
-            result.m_quit = ToQuit;
+            result.MoveTypeInfo = MoveType.CalculateMoveType(result);
+            result.m_quitIndicator = i_ToQuit;
 
             return result;
-
         }
 
         public bool WantsToQuitIndicator
         {
-            get { return m_quit; }
-            set { m_quit = value; }
+            get { return m_quitIndicator; }
+            set { m_quitIndicator = value; }
         }
+
         public Point CurrentPosition
         {
             get { return m_currentSoldierPosition; }
@@ -128,6 +124,7 @@ namespace Checkers_LogicAndDataSection
                 m_currentSoldierPosition.YCooord = value.YCooord;
             }
         }
+
         public Point RequestedPosition
         {
             get { return m_requestedSoldierPosition; }
@@ -137,7 +134,8 @@ namespace Checkers_LogicAndDataSection
                 m_requestedSoldierPosition.YCooord = value.YCooord;
             }
         }
-        public MoveType moveTypeInfo
+
+        public MoveType MoveTypeInfo
         {
             get { return m_moveInfo; }
             set
@@ -146,13 +144,14 @@ namespace Checkers_LogicAndDataSection
             }
         }
 
-        public bool Equals(CheckersGameStep step)
+        public bool Equals(CheckersGameStep i_step)
         {
             bool validity = true;
-            if (!(step.CurrentPosition.XCoord == CurrentPosition.XCoord && step.CurrentPosition.XCoord == CurrentPosition.XCoord && step.CurrentPosition.YCooord == CurrentPosition.YCooord && step.CurrentPosition.YCooord == CurrentPosition.YCooord))
+            if (!(i_step.CurrentPosition.XCoord == CurrentPosition.XCoord && i_step.CurrentPosition.XCoord == CurrentPosition.XCoord && i_step.CurrentPosition.YCooord == CurrentPosition.YCooord && i_step.CurrentPosition.YCooord == CurrentPosition.YCooord))
             {
                 validity = false;
             }
+
             return validity;
         }
     }
